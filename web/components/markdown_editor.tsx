58f1edf96d32c42cf068065cc61eae80fdd4e2e3
Elizabeth Coats <escoats@unc.edu>
2025-04-16 13:35:08 -0400

    Setup markdown editor card

---
+++ web/components/markdown_editor.tsx
@@ -0,0 +1,15 @@
+// Component that displays 
+
+import { Card } from "./ui/card";
+
+export function MarkdownEditor(pageId: string, ) {
+    // fetch data
+
+    
+    return (
+        <Card>
+            <p>Sample text</p>
+
+        </Card>
+    )
+}
