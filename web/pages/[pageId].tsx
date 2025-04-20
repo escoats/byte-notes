a1ea9ffff18638c5fc26a81a6f242acec694ab71
Elizabeth Coats <escoats@unc.edu>
2025-04-20 19:15:54 -0400

    Add null safety for pageId slug page

--- web/pages/[pageId].tsx
+++ web/pages/[pageId].tsx
@@ -1,13 +1,13 @@
-import { usePathname } from "next/navigation"
+import { usePathname } from "next/navigation";
 
 export default function Page() {
   const pathname = usePathname();
-  const pageId = pathname.slice(1)
+  const pageId = pathname?.slice(1);
   return (
     <div>
       <p>This is a published page!</p>
-      <p>page path: {pathname} </p>
-      <p>page id: {pageId}</p>
+      {pathname && <p>page path: {pathname}</p>}
+      {pageId && <p>page id: {pageId}</p>}
     </div>
   );
 }
