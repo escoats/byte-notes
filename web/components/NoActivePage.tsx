728cf2ecf0d1ba4119d79b61968cf8a984268d5a
Caitlin Estrada <caitlin.estrada@unc.edu>
2025-04-20 11:53:02 -0400

    updated no note selected page to resize logo and text by screen size and standardize notebook/chapter/page terminology

--- web/components/NoActivePage.tsx
+++ web/components/NoActivePage.tsx
@@ -3,10 +3,12 @@ import { FilePen } from "lucide-react";
 export function NoActivePage() {
   return (
     <div className="flex-1 flex flex-col justify-center items-center h-full">
-      <FilePen strokeWidth={1.5} className="h-[90px] w-[90px] m-4" />
-      <h1 className="font-bold text-lg mb-1 text-center">No Note Selected</h1>
-      <h2 className="font-bold text-gray-400 text-md max-w-[380px] text-center">
-        Select a note from the sidebar or create a new one to get started.
+      <FilePen strokeWidth={1.5} className="h-[10vh] w-[10vh] m-[2vh]" />
+      <h1 className="font-bold text-lg mb-[1vh] text-center">
+        No Page Selected
+      </h1>
+      <h2 className="font-bold text-gray-400 text-md max-w-[40vw] text-center">
+        Select a page from the sidebar or create a new one to get started.
       </h2>
     </div>
   );
