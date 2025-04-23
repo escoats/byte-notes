21ef7a57fe5d42512c87a4769279bc7ad78d6ff7
Caitlin Estrada <caitlin.estrada@unc.edu>
2025-04-22 22:26:22 -0400

    embedded code compiler with options to change language!

--- web/components/content/code-compiler.tsx
+++ web/components/content/code-compiler.tsx
@@ -1,26 +1,82 @@
 // Component that displays the code compiler card
 
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Button } from "../ui/button";
 import { Card, CardContent, CardHeader } from "../ui/card";
+import { createSupabaseComponentClient } from "@/utils/supabase/component";
 
-export function CodeCompiler(pageId: string) {
+const supabase = createSupabaseComponentClient();
+
+type CodeCompilerProps = {
+  activePageId: string;
+  defaultLanguage?: string;
+};
+
+const languages = [
+  "c",
+  "c#",
+  "c++",
+  "css",
+  "go",
+  "html",
+  "java",
+  "javascript",
+  "kotlin",
+  "python",
+  "r",
+  "ruby",
+  "rust",
+  "sql",
+  "swift",
+  "typescript",
+];
+
+export function CodeCompiler({
+  activePageId,
+  defaultLanguage = "python",
+}: CodeCompilerProps) {
+  const iframeRef = useRef<HTMLIFrameElement>(null);
+  const [language, setLanguage] = useState(defaultLanguage);
+  const [initialCode, setInitialCode] = useState<string>("");
+  const [currentCode, setCurrentCode] = useState<string>("");
+
+  // TODO: fetch page's code from supabase
+
+  // TODO: populate code compiler with code from supabase
+
+  // TODO: update current code on editor changes (not saved to supabase yet)
+  // TODO: could this be used as realtime?
+
   return (
     <div className="w-[50%] p-4">
       <Card>
-        <CardHeader>
-          <div className="flex justify-">
-            <Button variant="ghost">Edit</Button>
-            <Button variant="ghost">Run</Button>
-          </div>
-        </CardHeader>
-        <CardContent>
-          <p>
-            This is a placeholder Code Compiler view for the page with ID{" "}
-            {pageId}!
-          </p>
-          <p>Editing functionality & styling coming in Sprint 2!</p>
-        </CardContent>
+        <div className="flex items-center justify-between mb-2 ml-2 mr-2">
+          <label htmlFor="language" className="mr-2 font-medium-bold">
+            Language
+          </label>
+          <select
+            id="language"
+            value={language}
+            onChange={(e) => setLanguage(e.target.value)}
+            className="border rounded px-2 py-1"
+          >
+            {languages.map((lang) => (
+              <option key={lang} value={lang}>
+                {lang}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        {/* https://onecompiler.com/apis/embed-editor */}
+        <iframe
+          id="oc-editor"
+          ref={iframeRef}
+          frameBorder="0"
+          height="500px"
+          width="100%"
+          src={`https://onecompiler.com/embed/${language}?listenToEvents=true&codeChangeEvent=true`}
+        ></iframe>
       </Card>
     </div>
   );
