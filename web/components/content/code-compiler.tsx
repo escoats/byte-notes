48ef1992ee2f7b9e79668d345a098559976a0711
Charlotte Tsui <charlottetung@outlook.com>
2025-04-24 16:19:26 -0400

    light and dark mode working properly

--- web/components/content/code-compiler.tsx
+++ web/components/content/code-compiler.tsx
@@ -12,23 +12,21 @@ type CodeCompilerProps = {
 };
 
 export function CodeCompiler({ pageId, theme }: CodeCompilerProps) {
-  const embedId = `embed-${theme}`;
-
   useEffect(() => {
-    const container = document.getElementById(embedId);
+    const container = document.getElementById("embed");
     if (container) container.innerHTML = "";
 
-    sdk.embedProjectId(embedId, pageId, {
+    sdk.embedProjectId("embed", pageId, {
       clickToLoad: false,
       openFile: "index.ts",
-      theme,
+      theme: `${theme}`,
     });
   }, [pageId, theme]);
 
   return (
     <div className="w-[50%] px-6 py-4">
       <Card className="w-full max-w-5xl mx-auto h-[80.5%]">
-        <div id={`embed-${theme}`} className="h-full w-full" />
+        <div id="embed" className="h-full w-full" />
       </Card>
     </div>
   );
--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,14 +1,21 @@
 import "@/styles/globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "next-themes";
 
-// STEP 1: Create the object that will manage queries across the app.
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </ThemeProvider>
   );
 }
