637a24c132395f7462041e8be709b4d6cf2ea39a
Elizabeth Coats <escoats@unc.edu>
2025-04-24 12:04:32 -0400

    fix content update problems created by cursor fix

--- web/components/content/rich-text/editor.tsx
+++ web/components/content/rich-text/editor.tsx
@@ -32,7 +32,7 @@ const Editor = ({ content, placeholder, onChange }: EditorProps) => {
 
     if (!content) {
       editor.commands.clearContent();
-    } else if (editor?.isEmpty) {
+    } else if (editor.getHTML() !== content) {
       editor.commands.setContent(content, false, {
         preserveWhitespace: "full",
       });
