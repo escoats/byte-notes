f4afc9b07140f2f7e6e3f6aef596d88c5c244443
Caitlin Estrada <caitlin.estrada@unc.edu>
2025-04-24 16:55:42 -0400

    default markdown message

--- web/components/sidebar/new-button.tsx
+++ web/components/sidebar/new-button.tsx
@@ -121,6 +121,7 @@ export default function NewButton({
     const { error } = await supabase.from("page").insert({
       title: newPageTitle.trim(),
       chapter_id: selectedChapterId,
+      markdown: "Byte Notes",
     });
 
     if (error) return toast.error("Failed to create page.");
@@ -135,9 +136,7 @@ export default function NewButton({
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button className="mb-4 mr-2 ml-2 !bg-blue-400 ">
-          + New
-        </Button>
+        <Button className="mb-4 mr-2 ml-2 !bg-blue-400 ">+ New</Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-60">
         <DropdownMenuGroup>
